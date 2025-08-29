4308aedd02b8c8f689263f4eaf6225626bdfecd2
Avinash Barodiya <150107313+Avinash-barodia@users.noreply.github.com>
2025-08-29 13:01:31 +0000

    DataTypes

---
+++ 01-Basics/3-dataTypes.js
@@ -0,0 +1,13 @@
+"use strict";  //treat all JS code as newer version
+
+// alert(3+3) we are using nodejs, not browser
+
+console.log(3+3)
+console.log("Avinash") // code readability should be high
+
+// object
+let name = "Avinash";
+console.log(typeof "Avinash");
+console.log(typeof null);       // output: Object
+console.log(typeof undefined);  // output: undefined
+
