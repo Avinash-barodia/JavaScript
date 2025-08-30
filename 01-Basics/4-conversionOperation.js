9fc13aec7a5743ede7686eecfd5ea69b66ea4435
Avinash Barodiya <150107313+Avinash-barodia@users.noreply.github.com>
2025-08-30 03:01:49 +0000

    Operations

--- 01-Basics/4-conversionOperation.js
+++ 01-Basics/4-conversionOperation.js
@@ -23,3 +23,35 @@ let someNumber = 22;
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+// ********* Operations *********
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+
+// let str1 = "Hello"
+// let str2 = " Avinash"
+// console.log(str1+str2);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// prefix and postfix 
+let x = 100
+let y = x++;
+let a = 200
+let b = ++a;
+console.log(`x: ${x} & y: ${y}`);
+console.log(`a: ${a} & b: ${b}`);
+
